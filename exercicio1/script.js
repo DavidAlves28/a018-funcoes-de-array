0c7d6479e6bebd1c6db4ee3426ed6f43db3a465b
DavidAlves28 <dav.alvesc28@gmail.com>
2022-08-25 21:22:57 -0300

    Resolucao-David

--- exercicio1/script.js
+++ exercicio1/script.js
@@ -1,6 +1,41 @@
-const objeto = {
+const astro = {
     nome: "astrodev",
     profissao: "Dev das estrelas",
     username: "astrodev_labenu",
     senha: "melhorDeTodos"
 }
+//-----1-) PROPRIEDADES EM CAIXA ALTA
+const caixaAlta = (objeto) => {
+    const novoObjeto = { ...objeto }
+    for (let i in objeto) {
+        novoObjeto[i] = objeto[i].toUpperCase()
+    }
+    return novoObjeto
+}
+console.log(caixaAlta(astro))
+
+//------ 2) TEXTO CORRIDO
+
+const retornaTexto = (objeto) => {
+    let textoF = ``
+    for (let i in objeto) {
+        textoF = textoF + objeto[i]
+    }
+    console.log(textoF)
+    return textoF
+}
+console.log(retornaTexto(astro));
+
+// 3 ) funcao com callback
+
+const recebeCallback = (parametro1, callback) => {
+    const response = callback(parametro1)
+    return response
+}
+
+// a = callback com a funcao 1
+console.log(`callback funcao 1:`, recebeCallback(astro, caixaAlta))
+
+// b = callback com a funcao 2
+console.log(`callback funcao 2:`, recebeCallback(astro, retornaTexto))
+
--- exercicio3/script.js
+++ exercicio3/script.js
@@ -1,13 +1,25 @@
 const produtos = [
-    { nome: "Alface Lavada", categoria: "Hortifruti", preco: 2.5 },
-    { nome: "Guaraná 2l", categoria: "Bebidas", preco: 7.8 },
-    { nome: "Veja Multiuso", categoria: "Limpeza", preco: 12.6 },
-    { nome: "Dúzia de Banana", categoria: "Hortifruti", preco: 5.7 },
-    { nome: "Leite", categoria: "Bebidas", preco: 2.99 },
-    { nome: "Cândida", categoria: "Limpeza", preco: 3.30 },
-    { nome: "Detergente Ypê", categoria: "Limpeza", preco: 2.2 },
-    { nome: "Vinho Tinto", categoria: "Bebidas", preco: 55 },
-    { nome: "Berinjela kg", categoria: "Hortifruti", preco: 8.99 },
-    { nome: "Sabão em Pó", categoria: "Limpeza", preco: 10.80 }
- ]
- 
+   { nome: "Alface Lavada", categoria: "Hortifruti", preco: 2.5 },
+   { nome: "Guaraná 2l", categoria: "Bebidas", preco: 7.8 },
+   { nome: "Veja Multiuso", categoria: "Limpeza", preco: 12.6 },
+   { nome: "Dúzia de Banana", categoria: "Hortifruti", preco: 5.7 },
+   { nome: "Leite", categoria: "Bebidas", preco: 2.99 },
+   { nome: "Cândida", categoria: "Limpeza", preco: 3.30 },
+   { nome: "Detergente Ypê", categoria: "Limpeza", preco: 2.2 },
+   { nome: "Vinho Tinto", categoria: "Bebidas", preco: 55 },
+   { nome: "Berinjela kg", categoria: "Hortifruti", preco: 8.99 },
+   { nome: "Sabão em Pó", categoria: "Limpeza", preco: 10.80 }
+]
+//--- filter para retornar os nomes dos produtos de limpeza
+const produtosLimpeza = produtos.filter((nome) => {
+   if (nome.categoria === `Limpeza`)
+      console.log(nome.nome);
+   return
+})
+console.log(produtosLimpeza);
+
+
+
+
+
+
